86893201ceb17336769639c08f8ede37383e71fb
Ronald Chavez <ronaldchavezr@gmail.com>
2023-07-27 21:47:07 +0200

    final de la seccion  4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common'; // ofrece directivas como los ngif o ngfor, etc
 import { Routes, RouterModule } from '@angular/router';
+import { PagesRoutingModule } from './pages/pages.routing';
 
 // Rutas
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 
 // modulo de rutas
 
 
 // Creando una constante para mis rutas
 const routes: Routes = [
-  {
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch:'full' },
-    ]
-  },
-
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-
+  
+  // Documentacion
+  // path: '/dashboard', PagesRoutingModule
+  // path: '/auth', AuthRoutingModule
+  { path: '', redirectTo: 'dashboard', pathMatch:'full' },
   { path: '**', component: NopagefoundComponent },
 ];
 
@@ -37,7 +25,9 @@ const routes: Routes = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule,
   ],
   exports: [
     RouterModule
