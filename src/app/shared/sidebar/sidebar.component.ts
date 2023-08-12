7fe0273e11410d22ff33e1ec3352dac727e83ccc
Ronald Chavez <ronaldchavezr@gmail.com>
2023-08-12 22:21:06 +0200

    Implementacion de rutas en el sidebar de manera dinamica

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  menuItems: any[] = [];
+  longitud: number= 0;
+
+  constructor( private sideBar: SidebarService) {
+    this.menuItems = sideBar.menu;
+    console.log(this.menuItems);
+    this.longitud = this.sideBar.menu[0].submenu.length;
+    console.log(this.longitud);
+  }
+ 
 }
