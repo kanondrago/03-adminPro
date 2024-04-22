813439e5b0d31e909e05b7cd2a44972797c521cb
kanondrago <ronaldchavezr@gmail.com>
2024-04-22 20:25:43 +0200

    Repaso de observables

--- src/app/pages/promesas/promesas.component.ts
+++ src/app/pages/promesas/promesas.component.ts
@@ -9,11 +9,37 @@ export class PromesasComponent implements OnInit{
 
   resultado:string='';
 
+  usuarios: string[] = [];
+
   constructor() {
 
   }
 
   ngOnInit(): void {
 
+    this.getUsuarios()
+      .then((valor:any) => {
+        this.usuarios = valor
+      })
+
+
   }
+
+  getUsuarios() {
+
+    const url  = 'https://reqres.in/api/users?page=2';
+
+    const promesa = new Promise((resolve, reject) => {
+
+      fetch(url)
+        .then(resp => resp.json())
+        .then(body => resolve(body.data))
+        .catch(error => reject(error))
+
+    })
+
+    return promesa
+
+  }
+
 }
--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -11,125 +11,45 @@ export class RxjsComponent implements OnDestroy{
 
   // public intervalSubs: Subscription;
 
-  public intervalSubs: Subscription;
 
   constructor() {
 
-    // El observer es quien emite los valores
-
-    // this.returnObservable().pipe(retry(1)).subscribe({
-    //   next: (emision) => {
-    //     console.log(emision);
-    //   },
-    //   error: (error) => {
-    //     console.log(error);
-    //   },
-    // });
-
-    // this.intervalSubs = this.retornaInterval()
-    //   .subscribe( console.log )
-
-    this.intervalSubs = this.returnInterval().subscribe( console.log )
+    this.retornaObservable().pipe(
+      retry(1)
+    ).subscribe(
+      valor => console.log('subscripción: ', valor),
+      (error) => console.log('Error: ',error),
+      () => console.log('Info obs$ terminado')
+    )
 
   }
 
   ngOnDestroy(): void {
-    // throw new Error('Method not implemented.');
-    // this.intervalSubs.unsubscribe();
-    this.intervalSubs.unsubscribe();
   }
 
-  returnInterval(): Observable<number> {
+  retornaObservable(): Observable<number> {
+    let i = -1;
 
-    // take -> Toma todos los valores que se desee
-    // map -> Transforma la informacion que recibe el observable y la muta 
-    // de la manera que se necesite
+     return new Observable<number>(observer => {
 
-    const interval$ = interval(125)
-    .pipe(
-        map( valor => {
-          return valor+1;
-        }),
-        filter(valor => {
-          return (valor%3===0)? true: false
-        }),
-      );
-
-    return interval$;
-
-  }
-
-  returnObservable():Observable<number> {
-    let i =-1;
-
-    const obs$ = new Observable<number>((observer) => {
-      
-      
       const intervalo = setInterval(() => {
         i++;
-
         observer.next(i);
 
-        if (i===4) {
+        if(i===4) {
           clearInterval(intervalo);
           observer.complete();
         }
 
         if(i===2) {
-          observer.error('tenemos un error con i=2');
+          observer.error('Este es el error')
         }
 
-      }, 350)
-    });
-
-    return obs$;
-  }
-
-  retornaInterval(): Observable<string> {
-
-    // RXJS son Operadores que van en cadena
-
-    return interval(100)
-            .pipe(
-              map( valor => {
-                return (valor+1);
-              }),
-              filter((valor:any) => {
-                return ( (valor % 2 ) === 0 ) ? true: false;
-              })
-            )
-  }
-
-  retornaObservable(): Observable<number>{
-    let i = -1; // Para que no se inicie de nuevo
-
-    const obs$ = new Observable<number>( observer => {
-      
-      
-      const intervalo = setInterval(() => {
-        i++;
-        // Emitiendo el valor:
-        console.log(i);
-        observer.next(i);
-        
-
-        if(i===4) {
-          clearInterval(intervalo);
-          // Notificar que ya se cancelo el observable
-          observer.complete()
-        }
-
-        if(i===2) {
-          // Disparar el error
-          // console.log('i=2 .... error')
-          observer.error('la variable i llego al valor de 2')
-        }
 
       }, 1000)
 
-    } );
+    })
 
-    return obs$;
   }
 
 }
