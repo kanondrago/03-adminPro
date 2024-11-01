3520165d0ef292ac796154188ece2045888fb9d6
kanondrago <ronaldchavezr@gmail.com>
2024-11-01 12:42:08 +0100

    Creando Environments para los entornos local y prod

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RegisterForm } from '../interfaces/register-form.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class UsuarioService {
 
   constructor(private http: HttpClient) { }
 
-  crearUsuario(formData: any) {
+  crearUsuario(formData: RegisterForm) {
 
     console.log('Creando usuario ...', formData);
 
