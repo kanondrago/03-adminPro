0732e4b0d80308df80dae1fc715cdb1788d989d7
kanondrago <ronaldchavezr@gmail.com>
2024-11-01 11:04:31 +0100

    Validadores del formulario

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
@@ -20,7 +20,8 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule
+    RouterModule, // Para las rutas
+    ReactiveFormsModule, // Para formularios Reactivos
   ]
 })
 export class AuthModule { }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,71 @@ import { Component } from '@angular/core';
   styleUrls: [ './register.component.css']
 })
 export class RegisterComponent {
+
+  public formSumitted = false;
+
+  public registerForm = this.fb.group({
+    nombre: [ 'Ronald', [ Validators.required, Validators.minLength(3) ]],
+    email: [ 'ronald@mail.com', [ Validators.required ,Validators.email ] ],
+    password: [ '12345', [ Validators.required ] ],
+    password2: [ '12345', [ Validators.required ] ],
+    terminos: [ true, [ Validators.required ] ],
+  }, {  // Validadores ==> existen valores asincronos tambien
+    validators: this.passwordsIguales('password', 'password2')
+  })
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  crearUsuario() {
+
+    this.formSumitted = true;
+    console.log('Formulario: ',this.registerForm);
+
+    if(this.registerForm.valid) {
+      console.log('Posteando Formulario');
+    } else {
+      console.log('El Formulario no es correcto')
+    }
+  }
+
+  campoNoValido( campo: string ): boolean {
+
+    if( this.registerForm.get(campo)?.invalid && this.formSumitted ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  aceptaTerminos() {
+    return !this.registerForm.get('terminos')?.value && this.formSumitted;
+   }
+
+   contraseniasNoValidas(){
+      const pass1 = this.registerForm.get('password')?.value;
+      const pass2 = this.registerForm.get('password2')?.value;
+
+      if((pass1 !== pass2) && this.formSumitted) {
+        return true;
+      } else {
+        return false;
+      }
+   }
+
+   // Necesitamos retornar una función
+   passwordsIguales(pass1Name: string, pass2Name: string) {
+      return ( formGroup: FormGroup ) => {
+        const pass1Control = formGroup.get(pass1Name);
+        const pass2Control = formGroup.get(pass2Name);
+
+        if(pass1Control?.value === pass2Control?.value) {
+          pass2Control?.setErrors({ noEsIgual: false });
+        } else {
+          pass2Control?.setErrors({ noEsIgual: true })
+        }
+      }
+   }
 
 }
