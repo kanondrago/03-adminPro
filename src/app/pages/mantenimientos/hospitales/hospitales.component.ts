05a260ba53f26dc9848f13368569fe5f47871855
wolfrit <wolfrit.company@gmail.com>
2025-07-18 00:58:41 +0200

    Cargando hospitales, todo correcto

--- src/app/pages/mantenimientos/hospitales/hospitales.component.ts
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hospital } from 'src/app/models/hospital.model';
 import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
@@ -8,14 +9,26 @@ import { HospitalService } from 'src/app/services/hospital.service';
 })
 export class HospitalesComponent implements OnInit{
 
+  public hospitales: Hospital[] = [];
+  public cargando: boolean = true;
+
   constructor(private hospitalService: HospitalService) {
 
   }
 
   ngOnInit(): void {
+
+    this.cargarHospitales();
+  }
+
+  cargarHospitales(){
+
+    this.cargando = true;
     this.hospitalService.cargarHospitales()
-      .subscribe( resp => {
-        console.log('respondiendo .....: ',resp)
+      .subscribe( hospitales => {
+        this.cargando = false;
+        this.hospitales = hospitales;
       })
+      
   }
 }
